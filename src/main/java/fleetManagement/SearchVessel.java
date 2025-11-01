7669db763e9b83e4beea346c868edc9539605318
Muntasir Abdullah Mizan <muntasir.abdullah01@gmail.com>
2025-11-01 23:41:56 +0600

    search

--- src/main/java/fleetManagement/SearchVessel.java
+++ src/main/java/fleetManagement/SearchVessel.java
@@ -1,27 +1,22 @@
 package fleetManagement;
 import base.DriverManager;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class SearchVessel {
 
-    private static String webDriver = "Chrome";
     private static String vslName = "Indian Ocean";
     private static String vslStatus = "Maintenance";
     private static String vslType = "Tanker";
-    private static WebDriverWait wait;
-    private static WebDriver driver;
 
 
     public static void main(String [] args){
 
+        String webDriver = "Chrome";
         DriverManager.initDriver(webDriver);
         DriverManager.navigateURL(VesselData.marineExpress_URL);
-        wait = DriverManager.getWait();
-        driver = DriverManager.getDriver();
 
         AddVessel.openFleetManagementMenu();
         addedVvslSearchbyName(vslName);
@@ -31,12 +26,12 @@ public class SearchVessel {
     }
 
     private static void addedVslSearchButton(){
-        WebElement searchBtn = driver.findElement(SearchVesselLocator.addedVessel_Search_Button);
+        WebElement searchBtn = DriverManager.getDriver().findElement(SearchVesselLocator.addedVessel_Search_Button);
         searchBtn.click();
     }
     private static void addedVvslSearchbyName (String vslName){
 
-        WebElement searchVslbyName = wait.until(ExpectedConditions.visibilityOfElementLocated(SearchVesselLocator.addedVessel_Search_byName));
+        WebElement searchVslbyName = DriverManager.getWait().until(ExpectedConditions.visibilityOfElementLocated(SearchVesselLocator.addedVessel_Search_byName));
         searchVslbyName.sendKeys(vslName);
         addedVslSearchButton();
 
@@ -45,7 +40,7 @@ public class SearchVessel {
     private static void addedVslSearchbyStatus(String status){
 
         // WebElement srchByStatus = driver.findElement(AddVesselLocators.addedVessel_Search_byStatus);
-        WebElement srchByStatus = wait.until(ExpectedConditions.visibilityOfElementLocated(SearchVesselLocator.addedVessel_Search_byStatus));
+        WebElement srchByStatus = DriverManager.getWait().until(ExpectedConditions.visibilityOfElementLocated(SearchVesselLocator.addedVessel_Search_byStatus));
         Select select = new Select(srchByStatus);
         select.selectByValue(status);
         addedVslSearchButton();
@@ -53,7 +48,7 @@ public class SearchVessel {
 
     private static void addedVslSearchByTypes(String type){
 
-        WebElement vslSearchbyType = wait.until(ExpectedConditions.visibilityOfElementLocated(SearchVesselLocator.addedVessel_Search_byType));
+        WebElement vslSearchbyType = DriverManager.getWait().until(ExpectedConditions.visibilityOfElementLocated(SearchVesselLocator.addedVessel_Search_byType));
         Select select = new Select(vslSearchbyType);
         select.selectByValue(type);
         addedVslSearchButton();
